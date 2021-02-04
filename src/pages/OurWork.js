51942cad79e6513f5e9df31e05dd3773b452c2fa
Enzo <enzosantamaria@outlook.fr>
2021-02-04 22:04:00 +0100

    8. Added Framer motion

---
+++ src/pages/OurWork.js
@@ -0,0 +1,58 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import athlete from "../img/athlete-small.png";
+import theracer from "../img/theracer-small.png";
+import goodtimes from "../img/goodtimes-small.png";
+const OurWork = () => {
+  return (
+    <Work>
+      <Movie>
+        <h2>The athlete</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={athlete} alt="Athlete" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>The Racer</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={theracer} alt="racer" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>Good Times</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={goodtimes} alt="goodtimes" />
+        </Link>
+      </Movie>
+    </Work>
+  );
+};
+
+const Work = styled.div`
+  min-height: 100vh;
+  overflow: hidden;
+  background: white;
+  padding: 5rem 10rem;
+  h2 {
+    padding: 1rem 0rem;
+  }
+`;
+const Movie = styled.div`
+  padding-bottom: 10rem;
+  .line {
+    height: 0.5rem;
+    background: #cccccc;
+    margin-bottom: 3rem;
+  }
+  img {
+    object-fit: cover;
+    height: 70vh;
+    width: 100%;
+  }
+`;
+
+export default OurWork;
